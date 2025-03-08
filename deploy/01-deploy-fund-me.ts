3ba91e137e457575a5b486e671cd6dd2086916ca
John Titor <50095635+JohnRTitor@users.noreply.github.com>
2025-03-08 21:52:15 +0530

    add helper-hardhat-config.ts for ethUSD price feed dictionary
    get our price feed address from there
    
    Signed-off-by: John Titor <50095635+JohnRTitor@users.noreply.github.com>

--- deploy/01-deploy-fund-me.ts
+++ deploy/01-deploy-fund-me.ts
@@ -5,6 +5,7 @@
 import { DeployFunction } from "hardhat-deploy/dist/types";
 import { HardhatRuntimeEnvironment } from "hardhat/types";
 import { ethers, run, network } from "hardhat";
+import { networkConfig } from "../helper-hardhat-config";
 
 const deployFunc: DeployFunction = async function (
   hre: HardhatRuntimeEnvironment
@@ -12,15 +13,22 @@ const deployFunc: DeployFunction = async function (
   // destructure the functions, and variables from a parent interface/class
   // that we need to use them directly,
   // but we can always use `hre.getNamedAccounts()` though
-  const { deployments, getNamedAccounts } = hre;
+  const { deployments, getNamedAccounts, network } = hre;
   const { deploy, log } = deployments;
 
   // grab deployer account from getNamedAccounts()
   const { deployer } = await getNamedAccounts();
-  const chainId = network.config.chainId; // grab the chain id
 
+  const ethUSDPriceFeedAddress = networkConfig[network.name]["ethUsdPriceFeed"];
   // when going for localhost or hardhat network, we want to use a mock
   // of the price feed of ETH-USD https://docs.chain.link/data-feeds/price-feeds/addresses?network=ethereum&page=1#sepolia-testnet
+  const fundMe = await deploy("FundMe", {
+    // list of overrides we can apply to the constructor
+    from: deployer,
+    // pass price feed address as an argument
+    args: [ethUSDPriceFeedAddress],
+    log: true,
+  });
 };
 
 // export the function so `hardhat deploy` can call it
---
+++ helper-hardhat-config.ts
@@ -0,0 +1,25 @@
+// define an interface for network configuration items
+export interface networkConfigItem {
+  ethUsdPriceFeed?: string;
+  blockConfirmations?: number;
+}
+// define an interface for network configuration info using our interface
+export interface networkConfigInfo {
+  // keys are strings, meaning network names, and value is the
+  // networkConfigItem interface, containing ethUsdPriceFeed and blockConfirmations
+  [network: string]: networkConfigItem;
+}
+
+// let's actually define our network configuration info
+export const networkConfig: networkConfigInfo = {
+  localhost: {},
+  hardhat: {},
+  // Price Feed Address, values can be obtained at https://docs.chain.link/data-feeds/price-feeds/addresses
+  // Default one is ETH/USD contract on Sepolia
+  sepolia: {
+    ethUsdPriceFeed: "0x694AA1769357215DE4FAC081bf1f309aDC325306",
+    blockConfirmations: 6,
+  },
+};
+
+export const developmentChains = ["hardhat", "localhost"];
