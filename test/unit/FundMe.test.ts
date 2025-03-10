f8aab3831758f5bf4c24e550353bdc61fa71d2f9
John Titor <50095635+JohnRTitor@users.noreply.github.com>
2025-03-10 13:47:53 +0530

    tests: test fund function
    Signed-off-by: John Titor <50095635+JohnRTitor@users.noreply.github.com>

--- test/unit/FundMe.test.ts
+++ test/unit/FundMe.test.ts
@@ -1,12 +1,13 @@
 import { Contract } from "ethers";
 import { deployments, ethers, getNamedAccounts } from "hardhat";
-import { assert } from "chai";
+import { assert, expect } from "chai";
 import { AggregatorV3Interface, MockV3Aggregator } from "../../typechain-types";
+import { Address } from "hardhat-deploy/dist/types";
 
 describe("FundMe", async () => {
   let fundMe: Contract;
   let mockV3Aggregator: Contract;
-  let deployer: string;
+  let deployer: Address;
 
   beforeEach(async () => {
     // get the deployer account
@@ -31,12 +32,44 @@ describe("FundMe", async () => {
   });
 
   describe("constructor", async () => {
-    it("set the aggregator address correctly", async () => {
+    it("Set the aggregator address correctly", async () => {
       // read the priceFeed variable's value from the FundMe contract
       // priceFeed was of type AggregatorV3Interface, but now that it's deployed
       // it's Contract
-      const response: Contract = await fundMe.getFunction("priceFeed")();
-      assert.equal(response.address, mockV3Aggregator.address);
+      const priceFeedAggregatorAddress: Contract = await fundMe.getFunction(
+        "priceFeed"
+      )();
+      assert.equal(
+        priceFeedAggregatorAddress.address,
+        mockV3Aggregator.address
+      );
+    });
+  });
+
+  describe("fund", async () => {
+    // parseEther auto converts the provided ethereum to wei
+    const sendValue = ethers.parseEther("1"); // 1000000000000000000;
+
+    it("Fails if we don't send enough ETH", async () => {
+      await expect(fundMe.getFunction("fund")()).to.be.revertedWith(
+        "Didn't send enough"
+      );
+    });
+
+    it("Updates the addressToAmountFunded mapping", async () => {
+      await fundMe.getFunction("fund")({ value: sendValue });
+      // get the amount funded by the deployer
+      const amountFunded = await fundMe.getFunction("addressToAmountFunded")(
+        deployer
+      );
+      assert.equal(amountFunded.toString(), sendValue.toString());
+    });
+
+    it("Adds funder to array of funders", async () => {
+      await fundMe.getFunction("fund")({ value: sendValue });
+      // get our funder address
+      const funder: Address = await fundMe.getFunction("funders")(0);
+      assert.equal(funder, deployer);
     });
   });
 });
